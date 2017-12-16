7432d6195c501fdd144383dca3781cbcc8027052
jasoneffendi <jasoneffendi27@gmail.com>
2017-12-17 00:07:57 +0700

    Moved date function to helper and fixed writeFile missing callback

--- controllers/twitterController.js
+++ controllers/twitterController.js
@@ -1,33 +1,15 @@
 var oauth = require('../models/twitterAuth')
 var Excel = require('exceljs');
 var fs = require('fs');
+var formattedDate = require('../helpers/formattedDate')()
 var workbook = new Excel.Workbook();
 
 var newSheet = workbook.addWorksheet('Twitter Trends');
-var worksheet = workbook.getWorksheet('Twitter Trends')
+var worksheet = workbook.getWorksheet('Twitter Trends');
 class Twitter {
     static getTrends(req,res) {
         console.log(req.params.WOEID)
         var WOEID = req.params.WOEID || 23424977;
-        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var d = new Date();
-        var day = days[d.getDay()];
-        var hr = d.getHours();
-        var min = d.getMinutes();
-        if (min < 10) {
-            min = "0" + min;
-        }
-        var ampm = "am";
-        if( hr > 12 ) {
-            hr -= 12;
-            ampm = "pm";
-        }
-        var date = d.getDate();
-        var month = months[d.getMonth()];
-        var year = d.getFullYear();
-        var formattedDate = day + " " + hr + "." + min + ampm + " " + date + " " + month + " " + year;
-        console.log(formattedDate)
         oauth.get(
             `https://api.twitter.com/1.1/trends/place.json?id=${WOEID}`,
             '582131408-MdlJd3JcvSFQvCZYJC05w8RI6ukJvhSkl6w6h3tq', //test user token 
@@ -39,19 +21,21 @@ class Twitter {
                 var dict = [];
                 tweets.trends.forEach(trend => {
                     worksheet.addRow([trend.name, trend.query, trend.tweet_volume, tweets.as_of]);
-                    var obj = {"Trend" : trend.name,
-                    "Query" : trend.query,
-                    "Tweet Volume" : trend.tweet_volume,
-                    "Date" : tweets.as_of
+                    var obj = {'Trend' : trend.name,
+                    'Query' : trend.query,
+                    'Tweet Volume' : trend.tweet_volume,
+                    'Date' : tweets.as_of
                     }
                     dict.push(obj)
                 })
                 workbook.xlsx.writeFile(`./mined_data/spreadsheet/Twitter Trends(${tweets.locations[0].name})(${formattedDate}).xlsx`).then(function() {
-                    console.log("xls file is written.");
-                    res.send('xls file is written')            
+                    console.log(`Twitter Trends(${tweets.locations[0].name})(${formattedDate})` + 'has been saved')
+                    var dictstring = JSON.stringify(dict);                
+                    fs.writeFile(`./mined_data/json/Twitter Trends(${tweets.locations[0].name})(${formattedDate})` + ".json", dictstring, (err) => {
+                        if(err) throw err
+                        res.send(`Twitter Trends(${tweets.locations[0].name})(${formattedDate})` + 'has been saved')
+                    });                    
                 });
-                var dictstring = JSON.stringify(dict);
-                fs.writeFile(`./mined_data/json/Twitter Trends(${tweets.locations[0].name})(${formattedDate})` + ".json", dictstring);
             });
     }
 }
